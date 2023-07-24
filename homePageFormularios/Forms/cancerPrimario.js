32c6535557e19bcb8d8ed16516f9cb3b8b5a4c72
Cristina Placed <cristina.placed@estudiants.urv.cat>
2023-07-24 18:57:54 +0200

    Añadir cambio de color y heading dependiendo cp

---
+++ homePageFormularios/Forms/cancerPrimario.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heading = document.getElementById('headerTumorPrimario');
+    const radioInputs = document.querySelectorAll('input[name="cancerPrimario"]');
+
+    radioInputs.forEach(input => {
+        input.addEventListener("change", function() {
+            if (input.checked) {
+                switch (input.value) {
+                    case 'mama':
+                        heading.innerText = 'Tumor primario - Mama';
+                        heading.style.color = "#FF00FF"
+                        break;
+                    case 'pulmon':
+                        heading.innerText = 'Tumor primario - Pulmón';
+                        heading.style.color = "#FF0000"
+                        break;
+                    case 'colorrectal':
+                        heading.innerText = 'Tumor primario - Colorrectal';
+                        heading.style.color = "#B45F04"
+                        break;
+                    case 'prostata':
+                        heading.innerText = 'Tumor primario - Próstata';
+                        heading.style.color = "#04b404"
+                        break;
+                    default:
+                        heading.innerText = 'Tumor primario';
+                        break;
+                }
+            }
+        });
+    });
+});
